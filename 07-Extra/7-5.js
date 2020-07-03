c8b384bbb2cdc8e283a11259b80dcc70422513ba
Robin Jacquemart <jacquemart.rob@gmail.com>
2020-07-03 09:05:56 +0200

    7-5

--- 07-Extra/7-5.js
+++ 07-Extra/7-5.js
@@ -1,10 +1,14 @@
 let arr = ["Moto", "Voiture", "Velo", "Bus", "Trotinette"];
 console.log(arr);
 
-// for (i = 0; i < arr.length; i++) {
-//     if (arr[i] > arr[i + 1]) {
-//         return 1;
-//     }
-// }
+for(i = 0; i < arr.length; i ++) {
+    let last = arr[i];
+    let next = arr[i +1];
+    if(last > next ){
+        let arrTemp = last;
+        last = next;
+        next = arrTemp;
+    }
+}
 
 console.log(arr);
